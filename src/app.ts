f2d38e038a1081aeb70ded96b77d5cfa12553b4d
rafin8990 <rafin8990@gmail.com>
2023-11-09 18:26:45 +0600

    first commit

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express, { Application, NextFunction, Request, Response } from 'express';
+import cors from 'cors';
+
+const app: Application = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Travel Itinerary applicationh running succesfully ');
+});
+
+
+export default app;
