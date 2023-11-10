88f70eeb2b7ac1634b31886bd60c959fd6746a05
rafin8990 <rafin8990@gmail.com>
2023-11-10 22:00:34 +0600

    env

--- src/app/modules/user/user.interface.ts
+++ src/app/modules/user/user.interface.ts
@@ -1,28 +1,33 @@
 import { Model } from "mongoose";
 
 export enum BangladeshDistrict {
-    Dhaka = 'Dhaka',
-    Chattogram = 'Chattogram',
-    Rajshahi = 'Rajshahi',
-    Khulna = 'Khulna',
-    Barishal = 'Barishal',
-    Sylhet = 'Sylhet',
-    Rangpur = 'Rangpur',
-    Mymensingh = 'Mymensingh',
-    CoxBazar = 'CoxBazar',
-   
-  }
+  Dhaka = "Dhaka",
+  Chattogram = "Chattogram",
+  Rajshahi = "Rajshahi",
+  Khulna = "Khulna",
+  Barishal = "Barishal",
+  Sylhet = "Sylhet",
+  Rangpur = "Rangpur",
+  Mymensingh = "Mymensingh",
+  CoxBazar = "CoxBazar",
+}
 
-export type IUser= {
-    name: string;
-    dates: {
-      startDate: Date;
-      endDate: Date;
-    };
-    destinations: BangladeshDistrict[];
-    activities: string[];
-    transportationDetails: string;
-    accommodationDetails: string;
-  }
+export type IUser = {
+  name: string;
+  dates: {
+    startDate: Date;
+    endDate: Date;
+  };
+  destinations: BangladeshDistrict[];
+  activities: string[];
+  transportationDetails: string;
+  accommodationDetails: string;
+};
 
-  export type userModel = Model<IUser, Record<string, unknown>>;
+export type userModel = Model<IUser, Record<string, unknown>>;
+
+// something
+// something
+// something
+// something
+// something
