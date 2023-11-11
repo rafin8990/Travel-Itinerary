825887d2485b6c921b388e12604cf63f0795ed55
rafin8990 <rafin8990@gmail.com>
2023-11-11 11:03:29 +0600

    all interface added

--- src/app/modules/user/user.interface.ts
+++ src/app/modules/user/user.interface.ts
@@ -1,7 +1,5 @@
 import { Model } from "mongoose";
 
-<<<<<<< HEAD
-
 export type IUser= {
     phoneNumber:string;
     password:string;
@@ -13,36 +11,9 @@ export type IUser= {
     address:string;
 
   }
-=======
-export enum BangladeshDistrict {
-  Dhaka = "Dhaka",
-  Chattogram = "Chattogram",
-  Rajshahi = "Rajshahi",
-  Khulna = "Khulna",
-  Barishal = "Barishal",
-  Sylhet = "Sylhet",
-  Rangpur = "Rangpur",
-  Mymensingh = "Mymensingh",
-  CoxBazar = "CoxBazar",
-}
 
-export type IUser = {
-  name: string;
-  dates: {
-    startDate: Date;
-    endDate: Date;
-  };
-  destinations: BangladeshDistrict[];
-  activities: string[];
-  transportationDetails: string;
-  accommodationDetails: string;
-};
->>>>>>> 88f70eeb2b7ac1634b31886bd60c959fd6746a05
+
 
 export type userModel = Model<IUser, Record<string, unknown>>;
 
-// something
-// something
-// something
-// something
-// something
+
