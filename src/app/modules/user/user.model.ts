803ce1ed5bd29db82be31b0e13b64ae1f7963005
rafin8990 <rafin8990@gmail.com>
2023-11-15 13:09:33 +0600

    pre hook user added

--- src/app/modules/user/user.model.ts
+++ src/app/modules/user/user.model.ts
@@ -42,4 +42,30 @@ const userSchema = new Schema<IUser, Record<string, never>>(
   }
 );
 
+
+userSchema.methods.isUserExist = async function (
+  id: string,
+): Promise<Partial<IUser | null>> {
+  const user = await User.findOne(
+    { id },
+    { id: 1, password: 1, role: 1, needsPasswordChange: 1 },
+  );
+  return user;
+};
+
+userSchema.methods.isPasswordMatched = async function (
+  givenPassword: string,
+  savedPassword: string,
+): Promise<boolean> {
+  const isMatched = await bcrypt.compare(givenPassword, savedPassword);
+  return isMatched;
+};
+
+userSchema.pre('save', async function (next) {
+  this.password = await bcrypt.hash(
+    this.password,
+    Number(config.bycrypt_sault_round),
+  );
+  next();
+});
 export const User = model<IUser, userModel>("User", userSchema);
